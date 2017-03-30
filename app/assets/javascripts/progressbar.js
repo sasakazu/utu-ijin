d362dec2599d9b31cfe8a2e54ba31bf270b926e2
sasakazu <dropout.college2014@gmail.com>
2017-03-30 13:34:13 +0900

    progress bar done,,,,,,,,.....

---
+++ app/assets/javascripts/progressbar.js
@@ -0,0 +1,321 @@
+
+$(function() {
+  var $pb = $('.progress-bar:first');
+  var $pb2 = $('.progress-bar:last');
+
+  $("#items :checkbox").click(function() {
+    if($("#items :checked").length == 1) {
+
+      $pb.attr({
+        'style':'width:5%;',
+        'class':'progress-bar'
+      }).html(" 5% ");
+      }
+
+      else if($("#items :checked").length == 2) {
+      $pb.attr({
+        'style':'width:10%;',
+        'class':'progress-bar'
+      }).html(" 10% ");
+      }
+
+      else if($("#items :checked").length == 3) {
+      $pb.attr({
+        'style':'width:15%;',
+        'class':'progress-bar'
+      }).html(" 15% ");
+      }
+
+      else if($("#items :checked").length == 4) {
+      $pb.attr({
+        'style':'width:20%;',
+        'class':'progress-bar'
+      }).html(" 20% ");
+      }
+      else if($("#items :checked").length == 5) {
+      $pb.attr({
+        'style':'width:25%;',
+        'class':'progress-bar'
+      }).html(" 25% ");
+      }
+      else if($("#items :checked").length == 6) {
+      $pb.attr({
+        'style':'width:30%;',
+        'class':'progress-bar'
+      }).html(" 30% ");
+      }
+      else if($("#items :checked").length == 7) {
+      $pb.attr({
+        'style':'width:35%;',
+        'class':'progress-bar'
+      }).html(" 35% ");
+      }
+      else if($("#items :checked").length == 8) {
+      $pb.attr({
+        'style':'width:40%;',
+        'class':'progress-bar'
+      }).html(" 40% ");
+      }
+      else if($("#items :checked").length == 9) {
+      $pb.attr({
+        'style':'width:45%;',
+        'class':'progress-bar'
+      }).html(" 45% ");
+      }
+      else if($("#items :checked").length == 10) {
+      $pb.attr({
+        'style':'width:50%;',
+        'class':'progress-bar'
+      }).html(" 50% ");
+      }
+      else if($("#items :checked").length == 11) {
+      $pb.attr({
+        'style':'width:55%;',
+        'class':'progress-bar'
+      }).html(" 55% ");
+      }
+      else if($("#items :checked").length == 12) {
+      $pb.attr({
+        'style':'width:60%;',
+        'class':'progress-bar'
+      }).html(" 60% ");
+      }
+      else if($("#items :checked").length == 13) {
+      $pb.attr({
+        'style':'width:65%;',
+        'class':'progress-bar'
+      }).html(" 65% ");
+      }
+      else if($("#items :checked").length == 14) {
+      $pb.attr({
+        'style':'width:70%;',
+        'class':'progress-bar'
+      }).html(" 70% ");
+      }
+      else if($("#items :checked").length == 15) {
+      $pb.attr({
+        'style':'width:75%;',
+        'class':'progress-bar'
+      }).html(" 75% ");
+      }
+      else if($("#items :checked").length == 16) {
+      $pb.attr({
+        'style':'width:80%;',
+        'class':'progress-bar'
+      }).html(" 80% ");
+      }
+
+
+      else if($("#items :checked").length == 17) {
+      $pb.attr({
+        'style':'width:85%;',
+        'class':'progress-bar'
+      }).html(" 85% ");
+      }
+
+
+      else if($("#items :checked").length == 18) {
+      $pb.attr({
+        'style':'width:90%;',
+        'class':'progress-bar'
+      }).html(" 90% ");
+      }
+
+
+      else if($("#items :checked").length == 19) {
+      $pb.attr({
+        'style':'width:95%;',
+        'class':'progress-bar'
+      }).html(" 95% ");
+      }
+
+
+      else if($("#items :checked").length == 20) {
+      $pb.attr({
+        'style':'width:100%;',
+        'class':'progress-bar'
+      }).html(" 100% ");
+      }
+
+
+
+
+
+       else {
+      $pb.attr({
+        'style':'width:0%;',
+        'class':'progress-bar'
+      }).html(" 0% ");
+    }
+  });
+
+
+
+
+
+  $("#items2 :checkbox").click(function() {
+    if($("#items2 :checked").length == 1) {
+      $pb2.attr({
+        'style':'width:5%;',
+        'class':'progress-bar'
+      }).html(" 5% ");
+      }
+
+      else if($("#items2 :checked").length == 2) {
+      $pb2.attr({
+        'style':'width:10%;',
+        'class':'progress-bar'
+      }).html(" 10% ");
+      }
+
+
+       else if($("#items2 :checked").length == 3) {
+      $pb2.attr({
+        'style':'width:15%;',
+        'class':'progress-bar'
+      }).html(" 15% ");
+      }
+
+
+
+      else if($("#items2 :checked").length == 4) {
+      $pb2.attr({
+        'style':'width:20%;',
+        'class':'progress-bar'
+      }).html(" 20% ");
+      }
+
+
+       else if($("#items2 :checked").length == 5) {
+      $pb2.attr({
+          'style':'width:25%;',
+          'class':'progress-bar'
+      }).html(" 25% ");
+  }
+
+                  else if($("#items2 :checked").length == 6) {
+                  $pb2.attr({
+                    'style':'width:30%;',
+                    'class':'progress-bar'
+                  }).html(" 30% ");
+                  }
+
+
+                   else if($("#items2 :checked").length == 7) {
+                  $pb2.attr({
+                    'style':'width:35%;',
+                    'class':'progress-bar'
+                  }).html(" 35% ");
+                  }
+
+                        else if($("#items2 :checked").length == 8) {
+                        $pb2.attr({
+                          'style':'width:40%;',
+                          'class':'progress-bar'
+                        }).html(" 40% ");
+                        }
+
+
+                         else if($("#items2 :checked").length == 9) {
+                        $pb2.attr({
+                          'style':'width:45%;',
+                          'class':'progress-bar'
+                        }).html(" 45% ");
+                        }
+
+                              else if($("#items2 :checked").length == 10) {
+                              $pb2.attr({
+                                'style':'width:50%;',
+                                'class':'progress-bar'
+                              }).html(" 50% ");
+                              }
+
+
+                               else if($("#items2 :checked").length == 11) {
+                              $pb2.attr({
+                                'style':'width:55%;',
+                                'class':'progress-bar'
+                              }).html(" 55% ");
+                              }
+
+                                    else if($("#items2 :checked").length == 12) {
+                                    $pb2.attr({
+                                      'style':'width:60%;',
+                                      'class':'progress-bar'
+                                    }).html(" 60% ");
+                                    }
+
+
+                                     else if($("#items2 :checked").length == 13) {
+                                    $pb2.attr({
+                                      'style':'width:65%;',
+                                      'class':'progress-bar'
+                                    }).html(" 65% ");
+                                    }
+
+                                          else if($("#items2 :checked").length == 14) {
+                                          $pb2.attr({
+                                            'style':'width:70%;',
+                                            'class':'progress-bar'
+                                          }).html(" 70% ");
+                                          }
+
+
+                                           else if($("#items2 :checked").length == 15) {
+                                          $pb2.attr({
+                                            'style':'width:75%;',
+                                            'class':'progress-bar'
+                                          }).html(" 75% ");
+                                          }
+
+                                                else if($("#items2 :checked").length == 16) {
+                                                $pb2.attr({
+                                                  'style':'width:80%;',
+                                                  'class':'progress-bar'
+                                                }).html(" 80% ");
+                                                }
+
+
+                                                 else if($("#items2 :checked").length == 17) {
+                                                $pb2.attr({
+                                                  'style':'width:85%;',
+                                                  'class':'progress-bar'
+                                                }).html(" 85% ");
+                                                }
+
+
+                                             else if($("#items2 :checked").length == 18) {
+                                                $pb2.attr({
+                                              'style':'width:90%;',
+                                                                    'class':'progress-bar'
+                                                  }).html(" 90% ");
+                                                        }
+
+
+
+                                                        else if($("#items2 :checked").length == 19) {
+                                                        $pb2.attr({
+                                                          'style':'width:95%;',
+                                                          'class':'progress-bar'
+                                                        }).html(" 95% ");
+                                                        }
+
+
+                                                         else if($("#items2 :checked").length == 20) {
+                                                        $pb2.attr({
+                                                          'style':'width:100%;',
+                                                          'class':'progress-bar progress-bar-striped active'
+                                                        }).html(" 100% ");
+                                                        }
+
+
+
+
+       else {
+      $pb2.attr({
+        'style':'width:0%;',
+        'class':'progress-bar'
+      }).html(" 0% ");
+    }
+  });
+  });
